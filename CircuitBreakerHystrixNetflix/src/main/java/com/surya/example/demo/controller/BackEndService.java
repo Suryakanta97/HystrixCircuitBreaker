7ed9e5d614ca57dd3d6c0827e7030504e016f100
Suryakanta Sahoo <Suryakanta97@gmail.com>
2018-07-10 09:34:00 +0530

    Hystrix microservice Circuit breaker impl
    Here the impl of hystrix service is implemented

---
+++ CircuitBreakerHystrixNetflix/src/main/java/com/surya/example/demo/controller/BackEndService.java
@@ -0,0 +1,13 @@
+package com.surya.example.demo.controller;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class BackEndService {
+
+	public String getData() throws InterruptedException{
+		
+		Thread.sleep(15000);
+		return "HelloHystrix";
+	}
+}
---
+++ CircuitBreakerHystrixNetflix/src/main/java/com/surya/example/demo/controller/HystixImpl.java
@@ -0,0 +1,61 @@
+package com.surya.example.demo.controller;
+
+import java.net.URI;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
+
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+
+@Component
+public class HystixImpl {
+	
+	@Autowired
+	private BackEndService backEndService;
+	
+	@Autowired
+	private RestTemplate restTemplate;
+	
+	private ResponseEntity<String> exchange = null;
+	
+	@HystrixCommand(fallbackMethod = "defaultFallbackCallGetName")
+	public ResponseEntity<String> getName(){
+		try{
+			System.out.println("*********************Inside Main*******************");
+			URI endPoint = URI.create("http://localhost:8081/greeting");
+			return restTemplate.getForEntity(endPoint, String.class);
+		}catch(Exception e){
+			throw new RuntimeException();
+		}
+		
+	}
+	
+	public ResponseEntity<String> defaultFallbackCallGetName(){
+		System.out.println("*********************Inside fallback*******************");
+		URI endPoint = URI.create("http://localhost:8082/greeting");
+		return restTemplate.getForEntity(endPoint, String.class);
+		
+	}
+	
+	@HystrixCommand(fallbackMethod = "defaultFallbackCallGetData")
+	public String getData(){
+		try{
+			System.out.println("*********************Inside Main*******************");
+			return backEndService.getData();	
+		}catch(Exception e){
+			throw new RuntimeException();
+		}
+		
+		
+	}
+	
+	public String defaultFallbackCallGetData(){
+		System.out.println("*********************Inside fallback*******************");
+		return "fallback";
+		
+	}
+		
+		
+}
