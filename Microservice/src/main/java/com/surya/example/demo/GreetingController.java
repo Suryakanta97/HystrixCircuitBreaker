571071f5be573373650edc7a302db773ab967e74
Suryakanta Sahoo <Suryakanta97@gmail.com>
2018-07-10 09:30:34 +0530

    Microservice For hystrix
    Main Microservice which will work if it goes down the backup will work

---
+++ Microservice/src/main/java/com/surya/example/demo/GreetingController.java
@@ -0,0 +1,15 @@
+package com.surya.example.demo;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class GreetingController {
+
+    private static final String template = "Hello, %s!";
+
+    @RequestMapping("/greeting")
+    public String greeting() {
+    	return "Greetings from Spring Boot Microservice!";
+    }
+}
